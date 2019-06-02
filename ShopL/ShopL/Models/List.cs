d00b351b68755b570d358b7361d3cfe62c0de7eb
JerryMorris <j78morris@gmail.com>
2019-06-01 20:51:33 -0500

    fgfd
    dfgdfg

---
+++ ShopL/ShopL/Models/List.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ShopL.Models
+{
+    public class List{
+        public int Id { get; set; }
+        public int Quantity { get; set; }
+
+
+    }
+
+}
