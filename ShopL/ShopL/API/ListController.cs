16b719a7490355cc1c36f2f5e47c91f328966411
JerryMorris <j78morris@gmail.com>
2019-06-02 11:55:41 -0500

    Basic ng services complete

---
+++ ShopL/ShopL/API/ListController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using ShopL.Data;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace ShopL.API
+{
+    [Route("api/[controller]")]
+    public class ListController : Controller
+    {
+        private ApplicationDbContext _context;
+        public ListController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        // GET: api/<controller>
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var list = _context.Lists.ToList();
+            return Ok(list);
+        }
+
+        // GET api/<controller>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<controller>
+        [HttpPost]
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/<controller>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/<controller>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
--- ShopL/ShopL/ClientApp/src/app/services/list.service.ts
+++ ShopL/ShopL/ClientApp/src/app/services/list.service.ts
@@ -1,8 +1,28 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable()
 export class ListService {
+  url = '/api/list/';
+  constructor(private http: HttpClient) { }
 
-  constructor() { }
+  getList() {
+    return this.http.get(this.url)
+  }
 
+  addItem(data) {
+    return this.http.post(this.url, (data))
+  }
+
+  getItem(id) {
+    return this.http.get(this.url, id)
+  }
+
+  updateItem(data) {
+    return this.http.put(this.url,(data))
+  }
+
+  deleteItem(id) {
+    return this.http.delete(this.url, id)
+  }
 }
